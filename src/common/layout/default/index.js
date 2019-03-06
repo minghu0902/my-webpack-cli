50fb823821ff65c4ca4d98cb613c588c35ac95d4
liminghu <3173329910@qq.com>
2019-03-06 21:53:04 +0800

    update

--- src/common/layout/default/index.js
+++ src/common/layout/default/index.js
@@ -2,14 +2,10 @@ const headerTpl = require('../../components/header');
 const footerTpl = require('../../components/footer');
 const layoutTpl = require('./tmp.hbs');
 
-const defaultHeaderCfg = {
-  title: '首页'
-}
-
 const layout = {
   init: function({ content, headerCfg = {} }) {
     return layoutTpl({
-      header: headerTpl(Object.assign(defaultHeaderCfg, headerCfg)),
+      header: headerTpl(headerCfg),
       footer: footerTpl(),
       content
     })
---
+++ src/pages/index/index.ts
@@ -0,0 +1,4 @@
+import './style';
+
+// 如果开发环境设置了 splitChunks.chunks = 'all', 这里面的代码就不执行了，也不会自动刷新了
+console.log(1)
